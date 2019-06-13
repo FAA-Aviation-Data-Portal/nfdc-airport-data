a473d5c683e91c79a9026e5d68dc096bc86585a5
Forrest Desjardins <desjardinsfg@gmail.com>
2019-06-12 16:53:01 -0800

    Update examples

--- example.js
+++ example.js
@@ -1,17 +1,25 @@
 const airportData = require('./')
 
-airportData.facilities({ city: 'Anchorage' }).then(results => {
-  console.log(results[0])
-})
+const example = async () => {
+  const facilities = await airportData.facilities({ city: 'Anchorage' })
+  console.log(facilities[0])
 
-airportData.runways({ city: 'Anchorage' }).then(results => {
-  console.log(results[0])
-})
+  const runways = await airportData.runways({ city: 'Anchorage' })
+  console.log(runways[0])
 
-airportData.remarks({ city: 'Anchorage' }).then(results => {
-  console.log(results[0])
-})
+  const remarks = await airportData.remarks({ city: 'Anchorage' })
+  console.log(remarks[0])
 
-airportData.schedules({ city: 'Anchorage' }).then(results => {
-  console.log(results[0])
-})
+  const schedules = await airportData.schedules({ city: 'Anchorage' })
+  console.log(schedules[0])
+
+  console.log(await airportData.regions())
+  console.log(await airportData.districts())
+  console.log(await airportData.states())
+  console.log(await airportData.counties())
+  console.log(await airportData.cities())
+  console.log(await airportData.uses())
+  console.log(await airportData.certifications())
+}
+
+;(async () => example())()
